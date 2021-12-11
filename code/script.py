1a7bb9c2d9b1566cf114dda6cc4286a90ad4263c
HARSHITH KUMAR <harshithkumar40@gmail.com>
2021-12-11 17:49:27 +0530

    final submit

--- code/script.py
+++ code/script.py
@@ -1,3 +1,4 @@
+import pandas as pd
 
 
 def covid_vaccine(vaccination_status_files, user_meta_file, output_file):
@@ -12,4 +13,21 @@ def covid_vaccine(vaccination_status_files, user_meta_file, output_file):
     """
     # your code goes here.
     #  feel free to modify this entire script, except this function signature.
-    pass
+    vaccination_file_data = []
+    for file_path in vaccination_status_files:
+        vaccination_file_data.append(pd.read_csv(file_path, sep='\t'))
+
+    overall_vaccination_file_data = pd.concat(vaccination_file_data)
+    overall_vaccination_file_data['date'] = pd.to_datetime(overall_vaccination_file_data['date'], format='%d-%m-%Y')
+    filtered_overall_vaccination_file_data = overall_vaccination_file_data.query(
+        "date >= '2020-02-01' and date <= '2021-11-30' "
+        "and (vaccine == 'A' or vaccine == 'B' or vaccine == 'C') ")
+
+    user_meta_file_data = pd.read_csv(user_meta_file, sep='\t')
+    merged_data_vaccination_user = pd.merge(filtered_overall_vaccination_file_data, user_meta_file_data, on=['user'])
+    merged_data_vaccination_user = merged_data_vaccination_user.drop('date', axis=1)
+    unique_merged_data_vaccination_user = merged_data_vaccination_user.groupby(["city", "state", "vaccine", "gender"])\
+        .nunique().reset_index()
+    unique_merged_data_vaccination_user.columns = ['city', "state", "vaccine", "gender", "unique_vaccinated_people"]
+    unique_merged_data_vaccination_user.to_csv(output_file, index=False, sep="\t")
+
